f034b28166c581accfe14cb51be8684353df8d6d
programmingnuoh <myth0725@gmail.com>
2021-02-03 12:28:59 +0900

    authenticate_user!の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
       @prototypes = Prototype.includes(:user)
